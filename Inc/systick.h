d043decf6e581e255703088ec0e01342b6b28083
Fozface <tyler.hickling99@gmail.com>
2026-06-14 14:15:05 -0700

    updated systick to include useful functions

--- Inc/systick.h
+++ Inc/systick.h
@@ -3,16 +3,14 @@
 
 #include "stm32f0xx.h"
 #include <stdint.h>
-
 #include <stdbool.h>
 
 
-uint32_t time_now_ms(void);
-bool time_elapsed(uint32_t start, uint32_t delay);
+uint32_t systick_get_time(void);
 
+bool systick_has_elapsed(uint32_t start, uint32_t delay);
 
 void systick_init(void);
-
 
 void systick_delay(uint32_t ms);
 
--- Src/systick.c
+++ Src/systick.c
@@ -3,15 +3,27 @@
 
 static volatile uint32_t g_tick = 0;
 
-
-
-
-
-void time_init(void)
+void systick_init(void)
 {
     
     //Configure SysTick for 1ms tick
     SysTick_Config(SystemCoreClock / 1000); 
 
     g_tick = 0;
+}
+
+void SysTick_Handler(void)
+{
+    g_tick++;
+
+}
+
+uint32_t systick_get_time(void)
+{
+    return g_tick;
+}
+
+bool systick_has_elapsed(uint32_t start, uint32_t delay)
+{
+    return (uint32_t)(g_tick - start) >= delay;
 }
